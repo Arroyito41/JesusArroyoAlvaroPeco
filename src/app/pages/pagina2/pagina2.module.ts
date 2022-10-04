85c2282c3f16a316ded280d75725889e3083992e
Arroyito41 <jarroyo92@hotmail.es>
2022-10-04 12:43:27 +0200

    Tercer commit

--- src/app/pages/pagina2/pagina2.module.ts
+++ src/app/pages/pagina2/pagina2.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { Pagina2PageRoutingModule } from './pagina2-routing.module';
 
 import { Pagina2Page } from './pagina2.page';
+import { ComponentesModule } from 'src/app/componentes/componentes.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    Pagina2PageRoutingModule
+    Pagina2PageRoutingModule,
+    ComponentesModule
   ],
   declarations: [Pagina2Page]
 })
