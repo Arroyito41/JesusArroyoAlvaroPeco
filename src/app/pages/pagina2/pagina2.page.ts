0b3e4eae330663b55b56574ec4db9a68e2e22b84
Alvaro <peco_alvaro@hotmail.com>
2022-10-04 12:33:52 +0200

    hago commit

--- src/app/pages/pagina2/pagina2.page.ts
+++ src/app/pages/pagina2/pagina2.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-pagina2',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagina2.page.scss'],
 })
 export class Pagina2Page implements OnInit {
-
+  nombre: string = "Alvaro";
+  apellidos: string = "Peco";
+  localidad: string = "Ciudad Real";
+  estudios: string = "2DAM";
+  contraseña: string="";
   constructor() { }
 
   ngOnInit() {
