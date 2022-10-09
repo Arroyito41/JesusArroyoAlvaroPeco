ddaf540ac883cbfa73eab7aea94182ae466b065d
Alvaro <peco_alvaro@hotmail.com>
2022-10-09 12:19:05 +0200

    modifico el proyecto rellenando la pagina 2DAM

--- src/app/pages/dam2/dam2.page.ts
+++ src/app/pages/dam2/dam2.page.ts
@@ -12,4 +12,27 @@ export class Dam2Page implements OnInit {
   ngOnInit() {
   }
 
+  asignaturas=[
+
+    {nombre:"Desarrollo de Interfaces",
+  icono:"bar-chart-outline",
+checkbox: "checkbox1"},
+
+  {nombre: "Acceso a datos",
+  icono: "cloud-download-outline",
+  checkbox: "checkbox2"},
+
+  {nombre: "Programación de servicios y procesos",
+  icono: "desktop-outline",
+  checkbox: "checkbox3"},
+
+  {nombre: "Programación multimedia y dispoditivos móviles",
+  icono: "phone-portrait-outline",
+  checkbox: "checkbox4"},
+
+  {nombre: "Sistemas de gestión empresarial",
+  icono: "bar-chart-outline",
+  checkbox: "checkbox5"},
+  ]
+
 }
