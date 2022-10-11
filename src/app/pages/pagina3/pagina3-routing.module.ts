df768717708b30b484eb9a1323c05d02a57541d2
Arroyito41 <jarroyo92@hotmail.es>
2022-10-11 12:07:54 +0200

    Realizo commit tabs

--- src/app/pages/pagina3/pagina3-routing.module.ts
+++ src/app/pages/pagina3/pagina3-routing.module.ts
@@ -4,58 +4,35 @@ import { Routes, RouterModule } from '@angular/router';
 import { Pagina3Page } from './pagina3.page';
 
 const routes: Routes = [
+  
   {
-    path: '',
-    component: Pagina3Page,
-  },
-  {
-    path: 'tabs',
+    path: 'pagina3',
     component: Pagina3Page,
     children: [
       {
-        path: 'dam1',
-        children: [
-          {
-            path: '',
-            loadChildren: () => import('../dam1/dam1.module').then( m => m.Dam1PageModule)
-          },
-        ],
+        path: 'tab1',
+        loadChildren: () => import('../dam1/dam1.module').then( m => m.Dam1PageModule)
+      },
+      {
+        path: 'tab2',
+        loadChildren: () => import('../dam2/dam2.module').then( m => m.Dam2PageModule)
+      },
+      {
+        path: 'tab3',
+        loadChildren: () => import('../fct/fct.module').then( m => m.FctPageModule)
       },
       {
         path: '',
-        redirectTo: '',
-        pathMatch: 'full',
-      },
-      {
-        path: 'dam2',
-        children: [
-          {
-            path: '',
-            loadChildren: () => import('../dam2/dam2.module').then( m => m.Dam2PageModule)
-          },
-        ],
-      },
-      {
-        path: '',
-        redirectTo: '',
-        pathMatch: 'full',
-      },
-      {
-        path: 'fct',
-        children: [
-          {
-            path: '',
-            loadChildren: () => import('../fct/fct.module').then( m => m.FctPageModule)
-          },
-        ],
-      },
-      {
-        path: '',
-        redirectTo: '',
+        redirectTo: 'tab1',
         pathMatch: 'full',
       },
     ],
   },
+  {
+        path: '',
+        redirectTo: 'tab1',
+        pathMatch: 'full',
+  }
 ];
 
 @NgModule({
